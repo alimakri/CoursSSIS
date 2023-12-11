3a9ee0262c0038c5ef8d020de9c744af935718b2
Ali MAKRI <github@makri.fr>
2023-12-11 15:49:19 +0100

    A

--- Requetes/1.sql
+++ Requetes/1.sql
@@ -13,11 +13,14 @@ SIZE = 8192KB ,
 FILEGROWTH = 65536KB 
 )
 
-SELECT        
-	Person.Person.BusinessEntityID, 
-	Person.Person.FirstName, 
-	Person.Person.LastName, Person.Address.City
-FROM            
-	Person.Address 
-	INNER JOIN Person.BusinessEntityAddress ON Person.Address.AddressID = Person.BusinessEntityAddress.AddressID 
-	INNER JOIN Person.Person ON Person.BusinessEntityAddress.BusinessEntityID = Person.Person.BusinessEntityID
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[dbo].Personne') AND type in ('U'))
+BEGIN
+	CREATE TABLE Personne
+	(
+	[Id] bigint NOT NULL,
+	Nom [nvarchar](max) NOT NULL,
+	Prenom [nvarchar](max) NOT NULL,
+	Ville [nvarchar](max) NULL,
+	CONSTRAINT [PK_Personne] PRIMARY KEY CLUSTERED ([Id] ASC),
+	);
+END
